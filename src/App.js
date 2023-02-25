ccaa231bf337cc6768c266b398ad7dc49f258dd6
chiaping <chiapinghuang22@gmail.com>
2023-02-25 10:38:13 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
+import getWeatherData from './services/weatherService';
+import getFormattedWeatherData from './services/weatherService';
+import { useState, useEffect } from 'react';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+
+
 
 function App() {
+
+  const [query, setQuery] = useState({ q: 'berlin' });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fecthWeather = async () => {
+     await getFormattedWeatherData({...query, units}).then((data) => {
+        setWeather(data);
+     });
+    };
+
+    fecthWeather();
+  }, [query,units]);
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-purple-700 cyan h-fit shadow-xl shadow-gray-400">
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+      {weather && (
+      <div>
+      <TimeAndLocation weather={weather} />
+      <TemperatureAndDetails weather={weather} />
+      <Forecast title="hourly forecast" items={weather.hourly}/>
+      <Forecast title="daily forecast" items={weather.daily}/>
+      </div>
+      
+      )}
     </div>
+
   );
 }
 
